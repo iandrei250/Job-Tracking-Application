0e16b1ad0a79b559940331c8e63fb546147a0e1f
iandrei250 <aiordache020@gmail.com>
2025-05-29 14:41:43 +0300

    update: updated controller

--- tracker/src/main/java/job/tracker/tracker/controllers/ApplicationsController.java
+++ tracker/src/main/java/job/tracker/tracker/controllers/ApplicationsController.java
@@ -1,20 +1,45 @@
 package job.tracker.tracker.controllers;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.List;
+import java.util.UUID;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import job.tracker.tracker.entities.User;
+import job.tracker.tracker.entities.JobApplication;
+import job.tracker.tracker.service.JobService;
 
 @RestController
+@RequestMapping("/api/jobs")
 public class ApplicationsController {
-    private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
+    private final JobService jobService = new JobService();
 
-	@GetMapping("/greeting")
-	public User greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new User(counter.incrementAndGet(), String.format(template, name));
-	}
+       @GetMapping
+    public List<JobApplication> getAllJobs() {
+        return jobService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<JobApplication> getJobById(@PathVariable UUID id) {
+        return jobService.getById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<JobApplication> createJob(@RequestBody JobApplication job) {
+        return ResponseEntity.ok(jobService.create(job));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteJob(@PathVariable UUID id) {
+        boolean removed = jobService.delete(id);
+        return removed ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
+    }
 }
