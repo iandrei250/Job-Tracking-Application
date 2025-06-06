4bbc22dd9e8655c600b538872f9460bd840d6a85
iandrei250 <prtdace@gmail.com>
2025-06-06 17:33:20 +0300

    cors config

--- tracker/src/main/java/job/tracker/tracker/JobTrackerApplication.java
+++ tracker/src/main/java/job/tracker/tracker/JobTrackerApplication.java
@@ -2,12 +2,25 @@ package job.tracker.tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class JobTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobTrackerApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:8080");
+			}
+		};
 	}
 
 }
