a9d06605b9ba855a9553d1c543d8d342ced0dc57
iandrei250 <prtdace@gmail.com>
2025-07-02 12:03:17 +0200

    update: cleanup

--- tracker/src/main/java/job/tracker/tracker/service/UserService.java
+++ tracker/src/main/java/job/tracker/tracker/service/UserService.java
@@ -3,7 +3,6 @@ package job.tracker.tracker.service;
 import org.springframework.stereotype.Service;
 
 import job.tracker.tracker.db.UserRepository;
-import job.tracker.tracker.entities.JobApplicationEntity;
 import job.tracker.tracker.entities.UserEntity;
 
 import java.util.*;
